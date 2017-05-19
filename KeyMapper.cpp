ca4581f794d77eb14737ed69e99345d88e623ae4
Tim <ceoymw@gmail.com>
2017-05-19 08:58:49 +0200

    untabify???

--- KeyMapper.cpp
+++ KeyMapper.cpp
@@ -7,162 +7,162 @@
 
 bool KeyMapper::IsModifier(sf::Keyboard::Key key)
 {
-	 return (key == sf::Keyboard::Key::LShift ||
-		key == sf::Keyboard::Key::LControl ||
-		   key == sf::Keyboard::Key::LAlt ||
-		key == sf::Keyboard::Key::RControl ||	
-		key == sf::Keyboard::Key::RShift ||
-		key == sf::Keyboard::Key::RAlt);
+     return (key == sf::Keyboard::Key::LShift ||
+        key == sf::Keyboard::Key::LControl ||
+        key == sf::Keyboard::Key::LAlt ||
+        key == sf::Keyboard::Key::RControl ||	
+        key == sf::Keyboard::Key::RShift ||
+        key == sf::Keyboard::Key::RAlt);
 }
 
 KeyMapper::KeyMapper()
 {
-	FILE* config = fopen("KeyMapper.config", "r");
-	int eventType, inputType, keyCode, modifier;
-	char keyName[20];
+    FILE* config = fopen("KeyMapper.config", "r");
+    int eventType, inputType, keyCode, modifier;
+    char keyName[20];
 
-	/* Load from File and add to Keys Map */
-	while(fscanf(config, "%s %i, %i, %i, %i", &keyName, &eventType, &inputType, &keyCode, &modifier) == 5)
-	{
-		Key key;
-		key.eventType = (sf::Event::EventType) eventType;
-		key.inputType = (KeyMapper::InputType) inputType;
-		
-		/* Load Key Or Mouse */
-		if (inputType != KeyMapper::InputType::MouseInput)
-			key.keyCode = (sf::Keyboard::Key) keyCode;
-		else
-			key.mouseButton = (sf::Mouse::Button) keyCode;
+    /* Load from File and add to Keys Map */
+    while(fscanf(config, "%s %i, %i, %i, %i", &keyName, &eventType, &inputType, &keyCode, &modifier) == 5)
+    {
+        Key key;
+        key.eventType = (sf::Event::EventType) eventType;
+        key.inputType = (KeyMapper::InputType) inputType;
+        
+        /* Load Key Or Mouse */
+        if (inputType != KeyMapper::InputType::MouseInput)
+            key.keyCode = (sf::Keyboard::Key) keyCode;
+        else
+            key.mouseButton = (sf::Mouse::Button) keyCode;
 
-		key.modifier = (sf::Keyboard::Key) modifier;
+        key.modifier = (sf::Keyboard::Key) modifier;
 
-		this->Keys[keyName] = key;
-	}
+        this->Keys[keyName] = key;
+    }
 
-	fclose(config);
+    fclose(config);
 }
 
 KeyMapper::~KeyMapper() { }
 
 bool KeyMapper::KeyPressed(std::string key, sf::Event e)
 {
-	Key k = this->Keys[key];
-	
-	/* sf::Keyboard::Unknown is the Default */
-	auto modifier = k.modifier != sf::Keyboard::Unknown ? (sf::Keyboard::isKeyPressed(k.modifier) || this->lastKey == k.modifier) : true;
-	 
-	/* Handle Mouse Event */
-	if (k.inputType == MouseInput &&
-		k.eventType == e.type &&
-		k.mouseButton == e.mouseButton.button && 
-		modifier == true)
-	{
-		this->lastKey = sf::Keyboard::Unknown;
-		return true;
-	}
+    Key k = this->Keys[key];
+    
+    /* sf::Keyboard::Unknown is the Default */
+    auto modifier = k.modifier != sf::Keyboard::Unknown ? (sf::Keyboard::isKeyPressed(k.modifier) || this->lastKey == k.modifier) : true;
+     
+    /* Handle Mouse Event */
+    if (k.inputType == MouseInput &&
+        k.eventType == e.type &&
+        k.mouseButton == e.mouseButton.button && 
+        modifier == true)
+    {
+        this->lastKey = sf::Keyboard::Unknown;
+        return true;
+    }
 
-	/* Handle Keyboard Event */
-	if (k.inputType == KeyboardInput &&
-		k.eventType == e.type &&
-		k.keyCode == e.key.code &&
-		modifier)
-	{
-		this->lastKey = sf::Keyboard::Unknown;
-		return true;
-	}
+    /* Handle Keyboard Event */
+    if (k.inputType == KeyboardInput &&
+        k.eventType == e.type &&
+        k.keyCode == e.key.code &&
+        modifier)
+    {
+        this->lastKey = sf::Keyboard::Unknown;
+        return true;
+    }
 
-	/* Handle Modifier */
-	if (e.type == sf::Event::EventType::KeyPressed &&
-		this->IsModifier(e.key.code))
-	{
-		this->lastKey = e.key.code;
-	}
+    /* Handle Modifier */
+    if (e.type == sf::Event::EventType::KeyPressed &&
+        this->IsModifier(e.key.code))
+    {
+        this->lastKey = e.key.code;
+    }
 
-	return false;
+    return false;
 }
 
 bool KeyMapper::Save(std::string key, sf::Event e)
 {
-	bool success = false;
+    bool success = false;
 
-	/* Update Key */
-	Key updatedKey = this->Keys[key];
+    /* Update Key */
+    Key updatedKey = this->Keys[key];
 
-	/* Check for MouseButton */
-	if (e.type == sf::Event::EventType::MouseButtonPressed)
-	{
-		updatedKey.eventType = e.type;
-		updatedKey.inputType = MouseInput;
-		updatedKey.mouseButton = e.mouseButton.button;
-		updatedKey.modifier = this->lastKey;
+    /* Check for MouseButton */
+    if (e.type == sf::Event::EventType::MouseButtonPressed)
+    {
+        updatedKey.eventType = e.type;
+        updatedKey.inputType = MouseInput;
+        updatedKey.mouseButton = e.mouseButton.button;
+        updatedKey.modifier = this->lastKey;
 
-		/* Reset last key */
-		this->lastKey = sf::Keyboard::Unknown;
-		
-		success = true;
-	}
+        /* Reset last key */
+        this->lastKey = sf::Keyboard::Unknown;
+        
+        success = true;
+    }
 
-	/* Check for KeyPressed*/
-	if (e.type == sf::Event::EventType::KeyPressed)
-	{
-		/* Ignore Modifier Keys */
-		if (this->IsModifier(e.key.code))
-		{
-			this->lastKey = e.key.code;
-		}
+    /* Check for KeyPressed*/
+    if (e.type == sf::Event::EventType::KeyPressed)
+    {
+        /* Ignore Modifier Keys */
+        if (this->IsModifier(e.key.code))
+        {
+            this->lastKey = e.key.code;
+        }
 
-		updatedKey.eventType = e.type;
-		updatedKey.inputType = KeyboardInput;
-		updatedKey.keyCode = e.key.code;
-		updatedKey.modifier = this->lastKey;
+        updatedKey.eventType = e.type;
+        updatedKey.inputType = KeyboardInput;
+        updatedKey.keyCode = e.key.code;
+        updatedKey.modifier = this->lastKey;
 
-		/* Reset last key */
-		this->lastKey = sf::Keyboard::Unknown;
+        /* Reset last key */
+        this->lastKey = sf::Keyboard::Unknown;
 
-		success = true;
-	}
+        success = true;
+    }
 
-	if (success)
-		this->Keys[key] = updatedKey;
+    if (success)
+        this->Keys[key] = updatedKey;
 
-	return success;
+    return success;
 }
 
 void KeyMapper::WriteFile()
 {
-	FILE* config = fopen("KeyMapper.config", "w");
+    FILE* config = fopen("KeyMapper.config", "w");
 
-	for (auto const& x : this->Keys)
-	{
-		auto value = x.first;
-		auto key = x.second;
+    for (auto const& x : this->Keys)
+    {
+        auto value = x.first;
+        auto key = x.second;
 
-		/* Save Key Or Mouse */
-		int keyCode;
-		if (key.inputType != KeyMapper::InputType::MouseInput)
-			keyCode = (int)key.keyCode;
-		else
-			keyCode = (int)key.mouseButton;
+        /* Save Key Or Mouse */
+        int keyCode;
+        if (key.inputType != KeyMapper::InputType::MouseInput)
+            keyCode = (int)key.keyCode;
+        else
+            keyCode = (int)key.mouseButton;
 
-		std::string line = (value + " " +
-			std::to_string((int)key.eventType) + ", " +
-			std::to_string((int)key.inputType) + ", " +
-			std::to_string(keyCode) + ", " +
-			std::to_string((int)key.modifier));
+        std::string line = (value + " " +
+            std::to_string((int)key.eventType) + ", " +
+            std::to_string((int)key.inputType) + ", " +
+            std::to_string(keyCode) + ", " +
+            std::to_string((int)key.modifier));
 
-		const char* c = line.c_str();
+        const char* c = line.c_str();
 
-		fprintf(config, "%s\n", c);
-	}
-	fclose(config);
+        fprintf(config, "%s\n", c);
+    }
+    fclose(config);
 }
 
 
 /* 
-	Open Points:
-	
-	- Key is already bound to something
-	- Exception Handling
-	- Must-Have Keys
+    Open Points:
+    
+    - Key is already bound to something
+    - Exception Handling
+    - Must-Have Keys
 */
 
--- KeyMapper.h
+++ KeyMapper.h
@@ -11,78 +11,78 @@
 class KeyMapper
 {	
 private:	
-	/// <summary>
-	/// The last Key pressed
-	/// </summary>
-	sf::Keyboard::Key lastKey = sf::Keyboard::Key::Unknown;
-	
-	/// <summary>
-	/// Determines whether the specified key is modifier.
-	/// </summary>
-	/// <param name="key">The key.</param>
-	/// <returns>
-	///   <c>true</c> if the specified key is a modifier; otherwise, <c>false</c>.
-	/// </returns>
-	bool IsModifier(sf::Keyboard::Key key);
+    /// <summary>
+    /// The last Key pressed
+    /// </summary>
+    sf::Keyboard::Key lastKey = sf::Keyboard::Key::Unknown;
+    
+    /// <summary>
+    /// Determines whether the specified key is modifier.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns>
+    ///   <c>true</c> if the specified key is a modifier; otherwise, <c>false</c>.
+    /// </returns>
+    bool IsModifier(sf::Keyboard::Key key);
 
 public: 
-	/// <summary>
-	/// InputType Enum
-	/// </summary>
-	enum InputType
-	{
-		KeyboardInput,
-		MouseInput,
-		JoystickInput	
-	};
+    /// <summary>
+    /// InputType Enum
+    /// </summary>
+    enum InputType
+    {
+        KeyboardInput,
+        MouseInput,
+        JoystickInput	
+    };
   
-	/// <summary>
-	/// Key Structure
-	/// </summary>
-	struct Key
-	{
-		Key(): modifier(sf::Keyboard::Unknown) { };
-		InputType inputType;
-		sf::Event::EventType eventType;
-		sf::Keyboard::Key keyCode;
-		sf::Mouse::Button mouseButton;
-		sf::Keyboard::Key modifier;
-	};
+    /// <summary>
+    /// Key Structure
+    /// </summary>
+    struct Key
+    {
+        Key(): modifier(sf::Keyboard::Unknown) { };
+        InputType inputType;
+        sf::Event::EventType eventType;
+        sf::Keyboard::Key keyCode;
+        sf::Mouse::Button mouseButton;
+        sf::Keyboard::Key modifier;
+    };
   
-	/// <summary>
-	/// Map for the Key and the Keyaction Name ("Jump")
-	/// </summary>
-	std::map<std::string, Key> Keys;
-	
-	/// <summary>
-	/// Initializes a new instance of the <see cref="KeyMapper"/> class.
-	/// </summary>
-	KeyMapper();
+    /// <summary>
+    /// Map for the Key and the Keyaction Name ("Jump")
+    /// </summary>
+    std::map<std::string, Key> Keys;
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="KeyMapper"/> class.
+    /// </summary>
+    KeyMapper();
 
 
-	/// <summary>
-	/// Finalizes an instance of the <see cref="KeyMapper"/> class.
-	/// </summary>
-	~KeyMapper();
-		
-	/// <summary>
-	/// Check if the Key / Mouse linked to the Keyname is Pressed.
-	/// </summary>
-	/// <param name="key">The Keyaction name (for Example "Jump").</param>
-	/// <param name="e">The Event</param>
-	/// <returns>Result of the Evaluation</returns>
-	bool KeyPressed(std::string key, sf::Event e);
+    /// <summary>
+    /// Finalizes an instance of the <see cref="KeyMapper"/> class.
+    /// </summary>
+    ~KeyMapper();
+        
+    /// <summary>
+    /// Check if the Key / Mouse linked to the Keyname is Pressed.
+    /// </summary>
+    /// <param name="key">The Keyaction name (for Example "Jump").</param>
+    /// <param name="e">The Event</param>
+    /// <returns>Result of the Evaluation</returns>
+    bool KeyPressed(std::string key, sf::Event e);
 
-	/// <summary>
-	/// Saves the specified key.
-	/// </summary>
-	/// <param name="key">The Keyaction name (for Example "Jump").</param>
-	/// <param name="e">The Event</param>
-	/// <returns>Result of the Save</returns>
-	bool Save(std::string key, sf::Event e);
-	
-	/// <summary>
-	/// Writes the Config File.
-	/// </summary>
-	void WriteFile();
+    /// <summary>
+    /// Saves the specified key.
+    /// </summary>
+    /// <param name="key">The Keyaction name (for Example "Jump").</param>
+    /// <param name="e">The Event</param>
+    /// <returns>Result of the Save</returns>
+    bool Save(std::string key, sf::Event e);
+    
+    /// <summary>
+    /// Writes the Config File.
+    /// </summary>
+    void WriteFile();
 };
